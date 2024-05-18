10784906cab0f5d15d19defbdf00be3d487f47c5
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-05-18 18:32:56 -0400

    make get request for getting files and fix response for uploading file

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, abort
+from flask import Flask, request, send_file, abort, jsonify
 from flask_cors import CORS
 import uuid
 import boto3
@@ -51,9 +51,11 @@ def upload():
             filename = str(file_uuid) + "_" + file.filename
             s3.upload_fileobj(file, "resumenetworkmainresumebucket", filename)
             presigned_url = create_presigned_url(filename)
-            return "File successfully uploaded", 200
+            return jsonify({
+                "s3URL" : presigned_url,
+            }), 200
         else:
-            return "File type not allowed", 400
+            return abort(400, "File type not allowed!")
 
 
 # get the pdf file from the s3 bucket
@@ -64,11 +66,14 @@ def getFile():
     if not s3URL:
         return abort(400, "Missing s3 URL in the query parameter")
 
-    match = re.match(r'^https://(.+?)\.s3\.(.+?)\.amazonaws\.com/(.+)$', s3URL)
-    if not match:
+    parts = s3URL.split("/")
+
+    if not parts:
         return abort(400, "Invalid S3 URL format.")
-    
-    bucketName, objectKey = match.groups()
+
+    # Extract bucket name and object key
+    bucketName = parts[2].split(".")[0]
+    objectKey = "/".join(parts[3:])
 
     try:
         s3Object = s3.get_object(Bucket=bucketName, Key=objectKey)
@@ -78,17 +83,17 @@ def getFile():
         return send_file(
             fileStream,
             as_attachment=True,
-            attachment_filename=objectKey.split("/")[-1],
-            mimetype="application/pdf"
+            download_name=objectKey.split("/")[-1],
+            mimetype="application/pdf",
         )
-    
+
     except s3.exceptions.NoSuchKey:
         return abort(404, "File not found in the S3 Bucket")
-    except(NoCredentialsError, PartialCredentialsError):
+    except (NoCredentialsError, PartialCredentialsError):
         return abort(403, "AWS credentials not found or incomplete")
     except Exception as e:
         return abort(500, f"Something wrong happened in the server: {e}")
-        
+
 
 if __name__ == "__main__":
     app.run(debug=True)
