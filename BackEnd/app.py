d3c99d33043c46d7b401ed84a60266ea286659b5
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-05-19 02:12:48 -0400

    Finished process api that work with the LLMs

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -7,24 +7,9 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import os
 from dotenv import load_dotenv
 from io import BytesIO
-import re
 from BackEnd.neurelo.repos import Api
-import cohere
 
-COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
-
-def get_latex(prompts, file):
-    latex_as_string = file.read().decode('utf-8')
-    co = cohere.Client(api_key=COHERE_API_KEY)
-
-    full_prompt = "Output should be raw LaTex, 1 ready to be compiled and removed all comments. Don't include any comment. "+ prompts[0] + latex_as_string + " Output: ..."
-    response = co.chat(
-        model="command-r-plus",
-        message=full_prompt
-    )
-    result = response.text
-
-    return result
+from utils import get_latex, allowed_file, latex_to_pdf
 
 app = Flask(__name__)
 
@@ -37,23 +22,14 @@ s3 = boto3.client(
     config=Config(signature_version="s3v4", region_name="us-east-2"),
 )
 app.config["CORS_HEADERS"] = "Content-Type"
-ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif", "tex"}
-
 load_dotenv("./.env")
 
-
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# Use cloudfront presigned urls instead to avoid expiration?
 def create_presigned_url(s3_object_name: str):
     return s3.generate_presigned_url(
         "get_object",
         Params={"Bucket": os.environ.get("AWS_BUCKET_NAME"), "Key": s3_object_name},
         ExpiresIn=604800,
     )
-
 
 @app.route("/upload", methods=["POST", "OPTIONS", "GET"])
 def upload():
@@ -132,8 +108,13 @@ def postComments():
         comment, resumeId = data["comment"], data["resumeId"]
         
         # put this into the database
+        api.add_comments(resume_uuid=resumeId, content=comment) 
+        return jsonify({
+            "message" : "Comment saved sucessfully into the database"
+        }), 200
+
     except Exception as e:
-        return abort(500, f"Something wrong happened in the server!")
+        return abort(500, f"Something wrong happened in the server: {e}")
 
 @app.route("/get_comments/<resume_uuid>", methods=["GET"])
 def getComments(resume_uuid: str):
@@ -166,8 +147,22 @@ def process():
 
             # query the list of comments associate with the UUIDs from the database
             comments = api.get_comment_by_uuid(commentUUIDs)
+        
+            latexResult = get_latex(comments, fileStream)
+            
+            # convert this latex string to pdf file
+            outputFile = "output.pdf"
+            latex_to_pdf(latexResult, outputFile)
+            response = send_file(
+                latex_to_pdf,
+                as_attachment=True,
+                mimetype="application/pdf"
+            )
 
-            return get_latex(comments, fileStream)
+            # delete the temp pdf file after sending the response
+            os.remove(outputFile)
+
+            return response
 
         except s3.exceptions.NoSuchKey:
             return abort(404, "File not found in the S3 Bucket")
---
+++ BackEnd/utils.py
@@ -0,0 +1,40 @@
+import cohere
+import os
+import subprocess
+
+COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
+
+def get_latex(prompts, file):
+    latex_as_string = file.read().decode('utf-8')
+    co = cohere.Client(api_key=COHERE_API_KEY)
+
+    full_prompt = "Output should be raw LaTex, 1 ready to be compiled and removed all comments. Don't include any comment. "+ prompts[0] + latex_as_string + " Output: ..."
+    response = co.chat(
+        model="command-r-plus",
+        message=full_prompt
+    )
+    result = response.text
+
+    return result
+
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif", "tex"}
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def latex_to_pdf(latex_string, output_file):
+    try:
+        with open("tmep.tex", "w") as f:
+            f.write(latex_string)
+        
+        # Run pdflatex to convert .tex to .pdf
+        subprocess.run(["pdflatex", "-interaction=nonstopmode", "temp.tex"])
+
+        # Move the generated PDF to the desired output file path
+        subprocess.run(["mv", "temp.pdf", output_file])
+
+    finally:
+        # clean up the temp file
+        subprocess.run(["rm", "temp.tex", "temp.log", "temp.aux"])
+        
+
